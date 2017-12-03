0d996064f178f0a17962a4ff5760a507f8f6c581
connor.stroomberg <connorstroomberg@users.noreply.github.com>
2017-12-03 23:29:36 +0100

    setup for test with remote client driver

--- molgenis-selenium-tests/src/test/java/org/molgenis/selenium/tests/TestBaseSetup.java
+++ molgenis-selenium-tests/src/test/java/org/molgenis/selenium/tests/TestBaseSetup.java
@@ -3,11 +3,14 @@ package org.molgenis.selenium.tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.io.File;
 import java.io.UncheckedIOException;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
@@ -16,8 +19,13 @@ import java.util.Map;
 
 public class TestBaseSetup {
 
-	private WebDriver driver;
 	private final static Map<String, String> environmentVariables = System.getenv();
+
+	private static final String USERNAME = environmentVariables.get("selenium.sauce.user");
+	private static final String ACCESS_KEY = environmentVariables.get("selenium.sauce.key");
+	private static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+
+	private WebDriver driver;
 
 	public WebDriver getDriver() {
 		return driver;
@@ -30,6 +38,9 @@ public class TestBaseSetup {
 				break;
 			case "firefox":
 				driver = initFirefoxDriver(appURL);
+				break;
+			case "remote":
+				driver = initRemoteDriver(appURL);
 				break;
 			default:
 				System.out.println("browser : " + browserType
@@ -72,10 +83,33 @@ public class TestBaseSetup {
 		return driver;
 	}
 
+	private WebDriver initRemoteDriver(String appURL)
+	{
+		DesiredCapabilities caps = DesiredCapabilities.chrome();
+		caps.setCapability("platform", "Mac 10.12");
+		caps.setCapability("version", "62");
+		caps.setCapability("app", "appURL");
+
+
+		WebDriver driver = null;
+		try
+		{
+			driver = new RemoteWebDriver(new URL(URL), caps);
+		}
+		catch (MalformedURLException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+		driver.manage().window().maximize();
+		driver.navigate().to(appURL);
+		return driver;
+	}
+
 
 	@BeforeClass
 	public void initializeTestBaseSetup() {
-		String browserType = environmentVariables.getOrDefault("selenium.browser.type", "firefox");
+		String browserType = environmentVariables.getOrDefault("selenium.browser.type", "remote");
 		String appURL = environmentVariables.getOrDefault("selenium.app.url", "http://localhost:8080");
 		try {
 			setDriver(browserType, appURL);
