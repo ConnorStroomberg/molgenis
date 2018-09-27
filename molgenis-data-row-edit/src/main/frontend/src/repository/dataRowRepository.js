7f1f812767124354d182a74a637d0c8a8d546c1c
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2018-09-27 15:18:41 +0200

    Merge with master

--- molgenis-data-row-edit/src/main/frontend/src/repository/dataRowRepository.js
+++ molgenis-data-row-edit/src/main/frontend/src/repository/dataRowRepository.js
@@ -1,6 +1,10 @@
 import { EntityToFormMapper } from '@molgenis/molgenis-ui-form'
 import api from '@molgenis/molgenis-api-client'
 
+const entityMapperSettings = {
+  showNonVisibleAttributes: true
+}
+
 const isFileIncluded = (formData, formFields) => {
   const fieldsWithFile = formFields
     .filter((field) => field.type === 'file')
@@ -50,7 +54,7 @@ const parseEditResponse = (response) => {
 const fetchForCreate = (tableId) => {
   return new Promise((resolve, reject) => {
     api.get('/api/v2/' + tableId + '?num=0').then((response) => {
-      const mappedData = EntityToFormMapper.generateForm(response.meta, {}, { mapperMode: 'CREATE' })
+      const mappedData = EntityToFormMapper.generateForm(response.meta, {}, { mapperMode: 'CREATE', ...entityMapperSettings })
       resolve({formLabel: response.meta.label, ...mappedData})
     }, reject)
   })
@@ -60,7 +64,7 @@ const fetchForUpdate = (tableId, rowId) => {
   return new Promise((resolve, reject) => {
     api.get('/api/v2/' + tableId + '/' + rowId).then((response) => {
       const {_meta, rowData} = parseEditResponse(response)
-      const mappedData = EntityToFormMapper.generateForm(_meta, rowData, { mapperMode: 'UPDATE' })
+      const mappedData = EntityToFormMapper.generateForm(_meta, rowData, { mapperMode: 'UPDATE', ...entityMapperSettings })
       resolve({formLabel: response._meta.label, ...mappedData})
     }, reject)
   })
