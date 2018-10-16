e44bd3af73b62615814e18b1aeb56ac3b557bab6
connor <c.stroomberg@gmail.com>
2018-10-16 21:07:24 +0200

    Add some java doc describing why data is read into intermediate data structure, split of reading of data into named method

--- molgenis-one-click-importer/src/main/java/org/molgenis/oneclickimporter/service/impl/SpssServiceImpl.java
+++ molgenis-one-click-importer/src/main/java/org/molgenis/oneclickimporter/service/impl/SpssServiceImpl.java
@@ -30,29 +30,9 @@ public class SpssServiceImpl implements SpssService {
   public DataCollection fromSavFile(File file, String collectionName) throws IOException {
     SpssDataFileReader reader = spssDataFileReaderFactory.create(file);
 
-    List<SpssVariable> variables = reader.getVariables();
-    int colCount = variables.size();
-    int rowCount = reader.getNumCases() + 1; // plus 1 for header
+    List<List<Object>> dataTable = readData(reader);
 
-    // set header
-    List<List<Object>> dataTable = variables.stream().map(variable -> {
-      ArrayList<Object> column = new ArrayList<>(rowCount);
-      column.add(0, variable.getVariableName());
-      return column;
-    }).collect(Collectors.toList());
-
-
-    // set column data
-    while (reader.readNextCase()) {
-      IntStream.range(0, colCount)
-          .forEach(
-              colIndex -> {
-                Object cellValue = readValue(reader, variables.get(colIndex), colIndex);
-                dataTable.get(colIndex).add(cellValue);
-              });
-    }
-
-    // transform table data to list of columns
+    // Transform table data to list of columns for use in one click importer
     List<Column> columns =
         IntStream.range(0, dataTable.size())
             .mapToObj(
@@ -66,7 +46,47 @@ public class SpssServiceImpl implements SpssService {
     return DataCollection.create(collectionName, columns);
   }
 
-  private @Nullable Object readValue (SpssDataFileReader reader, SpssVariable variable, int colIndex) {
+  /**
+   * The Spss Reader reads the dataTable row by row. The OneClickImporter DataCollection takes a
+   * list of columns. Therefore we need to read the lines line by line and store them in a random
+   * access data structure (a list of lists).
+   *
+   * @param reader The Spss file reader, reads lines sequentially.
+   * @return Random access structure containing headers and data, List of columns with header at
+   *     index 0.
+   * @throws IOException may throw io exception when reading data from file.
+   */
+  private List<List<Object>> readData(SpssDataFileReader reader) throws IOException {
+    List<SpssVariable> variables = reader.getVariables();
+    final int colCount = variables.size();
+    final int rowCount = reader.getNumCases() + 1; // plus 1 for header
+
+    // Create random access data structure and read header data
+    List<List<Object>> dataTable =
+        variables
+            .stream()
+            .map(
+                variable -> {
+                  ArrayList<Object> column = new ArrayList<>(rowCount);
+                  column.add(0, variable.getVariableName());
+                  return column;
+                })
+            .collect(Collectors.toList());
+
+    // Read value data into the data structure
+    while (reader.readNextCase()) {
+      IntStream.range(0, colCount)
+          .forEach(
+              colIndex -> {
+                Object cellValue = readValue(reader, variables.get(colIndex), colIndex);
+                dataTable.get(colIndex).add(cellValue);
+              });
+    }
+    return dataTable;
+  }
+
+  private @Nullable Object readValue(
+      SpssDataFileReader reader, SpssVariable variable, int colIndex) {
     if (variable.isNumeric()) {
       final double doubleValue = reader.getDoubleValue(colIndex);
       return Double.isNaN(doubleValue) ? null : doubleValue;
